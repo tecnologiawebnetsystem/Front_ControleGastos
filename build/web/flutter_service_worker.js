5f39faabfaff01eeba88ea45bd8988c659b1d57e
Kleber Goncalves <kleber.goncalves.1209@gmail.com>
2025-03-05 00:30:01 -0300

    Subida

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "ce473afe8c873d14bb5ea78e40b80ee5",
+"flutter_bootstrap.js": "51d9b2570c1ff0afb24688d349cb1095",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
